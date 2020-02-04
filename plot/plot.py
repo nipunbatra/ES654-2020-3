043c9da58b876d6c01114480ac08f909d44580d9
Pankaj Pandey <pankaj.barco2012@gmail.com>
2020-02-04 09:53:50 +0530

    Add files via upload

---
+++ plot/plot.py
@@ -0,0 +1,22 @@
+class Plots():
+    def __init__(self):
+       
+        pass
+    
+    def surface_plot():
+        
+        "the surface plot of RSS in 3D"
+        pass
+    
+    def line_fit():
+        
+        
+        "line fit"
+        
+        pass
+        
+    def contour():
+        
+        "plot contour"
+        
+        pass
