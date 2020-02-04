6b798339513e0644daa8474f803aad26cb4fb025
Pankaj Pandey <pankaj.barco2012@gmail.com>
2020-02-04 09:54:30 +0530

    Add files via upload

---
+++ preprocessing/polynomial_features.py
@@ -0,0 +1,57 @@
+class PF():
+    
+    # use reference https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.PolynomialFeatures.html
+
+    def __init__(self, degree=2,include_bias=True):
+        
+        
+        pass
+ 
+    def fit():
+        
+        "Compute number of output features."
+        
+        pass
+    
+    def fit_transform():
+        
+        "Fit to data, then transform it."
+        
+        pass
+    
+    def get_feature_names():
+        
+        "Return feature names for output features"
+        
+        pass
+        
+    def get_params():
+        
+        "Get parameters for this estimator."
+        
+        pass
+    
+    def set_params():
+        
+        "Set the parameters of this estimator."
+        
+        pass
+    
+    
+    def transform():
+        
+        "Transform data to polynomial features"
+        
+        pass
+    
+        
+        
+        
+        
+        
+        
+        
+        
+    
+                
+                
