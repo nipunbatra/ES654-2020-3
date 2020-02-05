8ba2d473f5d4a134e6615a055e1d5b13032c70d2
Varun Gohil <gohil.varun@iitgn.ac.in>
2020-02-05 07:13:44 +0530

    Update q5_plot.py

--- q5_plot.py
+++ q5_plot.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib
-from preprocessing.polynomial_features import PF
+from preprocessing.polynomial_features import PolynomialFeatures
 
 x = np.array([i*np.pi/180 for i in range(60,300,4)])
 np.random.seed(10)  #Setting seed for reproducibility
