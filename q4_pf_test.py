453a5564a19fc85b6c2dece773b8c22a688d925e
Varun Gohil <gohil.varun@iitgn.ac.in>
2020-02-07 09:15:07 +0530

    Update q4_pf_test.py

--- q4_pf_test.py
+++ q4_pf_test.py
@@ -3,7 +3,6 @@ from preprocessing.polynomial_features import PolynomialFeatures
 
 
 
-X = np.arange(6).reshape(3, 2)
+X = np.array([1,2])
 poly = PolynomialFeatures(2)
 poly.transform(X)
-# assert((poly.transform(X) == np.array([[ 1.,  0.,  1.,  0.,  0.,  1.],[ 1.,  2.,  3.,  4.,  6.,  9.], [ 1.,  4.,  5., 16., 20., 25.]])).all())
